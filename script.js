bd9ad0024af03901c9f14a7cad513f3a78314a51
Kallestarsz <skhunters14@gmail.com>
2022-05-05 11:36:14 +0000

    Add playRound function and basic rps logic

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
- 
+let pick = ["rock","paper","scissors"]
+let playerSelection = prompt("Enter your pick")
+let computerSelection = pick[Math.round(Math.random()*2)]
+
+function playRound(playerSelection, computerSelection) {
+    let result = pick.indexOf(playerSelection) - pick.indexOf(computerSelection)
+    if (result == -1 || result == 2) { return "CPU won"}
+	else if (result == 0) { return "Tie!"}
+    else { return "You win"} 
+}
+console.log("You picked: ",playerSelection)
+console.log("CPU picked: ",computerSelection)
+console.log(playRound(playerSelection, computerSelection))
