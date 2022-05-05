589868a68ef0df1b6e49dbca7c994017b429ce1a
Kallestarsz <skhunters14@gmail.com>
2022-05-05 16:04:05 +0000

    Fix Syntax

--- script.js
+++ script.js
@@ -1,30 +1,34 @@
-let result,tie;
 const pick = ["rock","paper","scissors"]
+let result = 0
 
 function playRound(playerSelection, computerSelection) {
-    let result = pick.indexOf(playerSelection) - pick.indexOf(computerSelection)
-    if (result == -1 || result == 2) {  result--; return "CPU won";}
-	else if (result == 0) {  tie = true; return "Tie!";}
-    else { result++; return "You win"; } 
+    let res = pick.indexOf(playerSelection) - pick.indexOf(computerSelection)
+    if (res == -1 || res == 2) {  
+        result--;
+        return "CPU won"
+    }
+	else if (res == 0)  return "Tie!";
+    else {
+        result++
+        return "You win"  
+    }
+
 }
 
 function gameSequence(){
-    let playerSelection;
-    let computerSelection;
     let round = 1;
-    result = 0;
-    tie = false;
     while(round <= 5){
-        playerSelection = prompt(`Round ${round}\nEnter your pick`)
-        computerSelection = pick[Math.round(Math.random()*2)] 
-        if(pick.includes(playerSelection)){
-            alert(`You picked: ${playerSelection}\nCPU picked: ${computerSelection}\n${playRound(playerSelection,computerSelection)}`)
-            if(tie == false){ round++ }
-            else{ alert("Retry"); tie = false }
+        let playerSelection = prompt(`Round ${round}\nEnter your pick`).toLowerCase()
+        let computerSelection = pick[Math.floor(Math.random()*3)] 
+        let tie
+        if(playerSelection){
+            alert(`You picked: ${playerSelection}\nCPU picked: ${computerSelection}\n${tie =playRound(playerSelection,computerSelection)}`)
+            if(tie != "Tie!") round++
+            else alert("Retry")
         }
-        else { continue; }
+        else continue; 
     }
-    if (result > 0) { alert("You've won the game!") }
+    if (result > 0) alert("You've won the game!") 
     else { alert("You've lost the game!")}
 }
 
