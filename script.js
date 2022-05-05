6e8a0cbb70f7d48fcd7be1a3fe8032cd2dcf5ff0
Kallestarsz <skhunters14@gmail.com>
2022-05-05 14:25:36 +0000

    Add gameSequence function

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
-let pick = ["rock","paper","scissors"]
-let playerSelection = prompt("Enter your pick")
-let computerSelection = pick[Math.round(Math.random()*2)]
+let result,tie;
+const pick = ["rock","paper","scissors"]
 
 function playRound(playerSelection, computerSelection) {
     let result = pick.indexOf(playerSelection) - pick.indexOf(computerSelection)
-    if (result == -1 || result == 2) { return "CPU won"}
-	else if (result == 0) { return "Tie!"}
-    else { return "You win"} 
+    if (result == -1 || result == 2) {  result = false; return "CPU won";}
+	else if (result == 0) {  tie = true; return "Tie!";}
+    else { result = true; return "You win"; } 
 }
-console.log("You picked: ",playerSelection)
-console.log("CPU picked: ",computerSelection)
-console.log(playRound(playerSelection, computerSelection))
+
+function gameSequence(){
+    let playerSelection;
+    let computerSelection;
+    let round = 1;
+    result = 0;
+    tie = false;
+    while(round <= 5){
+        playerSelection = prompt(`Round ${round}\nEnter your pick`)
+        computerSelection = pick[Math.round(Math.random()*2)] 
+        if(pick.includes(playerSelection)){
+            alert(`You picked: ${playerSelection}\nCPU picked: ${computerSelection}\n${playRound(playerSelection,computerSelection)}`)
+            if(tie == false){ round++ }
+            else{ alert("Retry"); tie = false }
+        }
+        else { continue; }
+    }
+    if (result > 0) { alert("You've won the game!") }
+    else { alert("You've lost the game!")}
+}
+
+gameSequence();
