555c305a5cf75b92cc615e261c7fb80bac60f131
Kallestarsz <skhunters14@gmail.com>
2022-05-14 14:44:38 +0000

    Add UI

--- script.js
+++ script.js
@@ -1,35 +1,58 @@
 const pick = ["rock","paper","scissors"]
-let result = 0
+let round = 1
+let usersc = 0
+let cpusc = 0
+
 
 function playRound(playerSelection, computerSelection) {
-    let res = pick.indexOf(playerSelection) - pick.indexOf(computerSelection)
+    let res = playerSelection - pick.indexOf(computerSelection)
     if (res == -1 || res == 2) {  
-        result--
-        return "CPU won"
+        cpusc++
+        return "CPU won!"
     }
 	else if (res == 0)  return "Tie!"
     else {
-        result++
-        return "You win"  
+        usersc++
+        return "You win!"  
     }
 
 }
 
-function gameSequence(){
-    let round = 1
-    while(round <= 5){
-        let playerSelection = prompt(`Round ${round}\nEnter your pick`).toLowerCase()
-        let computerSelection = pick[Math.floor(Math.random()*3)] 
-        let tie
-        if(playerSelection){
-            alert(`You picked: ${playerSelection}\nCPU picked: ${computerSelection}\n${tie =playRound(playerSelection,computerSelection)}`)
-            if(tie != "Tie!") round++
-            else alert("Retry")
-        }
-        else continue
-    }
-    if (result > 0) alert("You've won the game!") 
-    else alert ("You've lost the game!")
-}
+ const pChoice = document.querySelector('.pChoice')
+ const cChoice = document.querySelector('.cChoice')
+ const output = document.querySelector('.output')
 
-gameSequence()
+ const pScore = document.querySelector('.pScore')
+ const cScore = document.querySelector('.cScore')
+
+ const roundCount = document.querySelector('.roundCount')
+
+function betterGameSequence(playerSelection){
+    let computerSelection = pick[Math.floor(Math.random()*3)] 
+    let tie
+    if(round <= 5){
+    pChoice.textContent = pick[playerSelection]
+    cChoice.textContent = computerSelection
+    output.textContent = tie = playRound(playerSelection,computerSelection)
+    roundCount.textContent = round
+
+    pScore.textContent = usersc
+    cScore.textContent = cpusc
+
+    if(tie != "Tie!") round++
+    }
+    else{
+    if (usersc >= cpusc) alert("You've won the game!") 
+    else alert("You've lost the game!")
+    round = 1
+    usersc = 0
+    cpusc = 0
+    }
+ } 
+
+const buttons = document.querySelectorAll('button')
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+   betterGameSequence(button.id)
+  });
+});
